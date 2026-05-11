c4200333c20d3c64651c7c59076976d6b9807223
CompuGlobalHyper <phineascrisp@gmail.com>
2026-05-11 13:02:53 -0700

    Added background image

--- ispy_frontend/src/routes/Home.jsx
+++ ispy_frontend/src/routes/Home.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import styles from './styles/Home.module.css'
 import Image from "../assets/ispypic.jpg"
+import Background from "../assets/background.png"
 
 function Home() {
     return (
         <>
-            <div className={styles.imageContainer}>
-                <img className={ styles.image } src={ Image } alt="" />
+            <div className={ styles.bckgrdImageContainer}>
+                <img className={ styles.bckgrdImage } src={ Background } alt="" />
+                <div className={styles.imageContainer}>
+                    <img className={ styles.image } src={ Image } alt="" />
+                </div>
             </div>
+            
         </>
     )
 }
